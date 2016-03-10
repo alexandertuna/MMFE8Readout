20f0e8c77e74b3f90cdf08b964aed9ff9cb47cfb
alexandertuna <alexander.tuna@gmail.com>
2016-03-10 12:58:03 -0500

    put standalone functions here

---
+++ python/dev/tuna/helpers.py
@@ -0,0 +1,9 @@
+def convert_to_int(list_of_bits):
+    this = "0b"
+    for bit in list_of_bits:
+        this += str(bit)
+    return int(this, base=2)
+
+def convert_to_32bit(list_of_bits):
+    return sum([int(list_of_bits[bit])*pow(2, bit) for bit in xrange(32)])
+
