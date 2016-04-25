7266efdd51618cad27e10d82a3c00c3aae2641dc
sezata <sezata@gmail.com>
2016-04-25 16:49:27 -0400

    changing toplevel for real ext trig input

--- srcs/sources_1/src/toplevel.vhd
+++ srcs/sources_1/src/toplevel.vhd
@@ -760,8 +760,8 @@ architecture STRUCTURE of toplevel is
     signal clk_tp_period_cnt    : std_logic_vector(19 downto 0) := x"007D0";--20 us
     signal clk_tp_dutycycle_cnt : std_logic_vector(19 downto 0) := x"003E8";--10 us
 
-    signal clk_tp_period_cnt_calib    : std_logic_vector(19 downto 0) := x"30D40"; --1 kHz
-    signal clk_tp_dutycycle_cnt_calib : std_logic_vector(19 downto 0) := x"186A0"; -- 500 microseconds
+    signal clk_tp_period_cnt_calib    : std_logic_vector(19 downto 0) := x"F4240"; --1 kHz
+    signal clk_tp_dutycycle_cnt_calib : std_logic_vector(19 downto 0) := x"61A80"; -- 500 microseconds
 
     signal counter_for_cktp_done : std_logic_vector(15 downto 0) := x"0000";
     signal cktp_done             : std_logic                     := '0';
@@ -1469,15 +1469,12 @@ begin
     U_cktp_gen : process(clk_100, reset)
     begin
         if rising_edge(clk_100) then
-            if ((reset = '1' or ext_trigger_edge= '1') and (ext_trigger_in_sel = '1')) then --
-              -- reset=1 or (                           and         ) should be
-              -- paolo 
---            if (int_trig_edge = '1' or  reset = '1') then
+            if ((reset = '1' or ext_trigger_edge= '1') and (ext_trigger_in_sel = '1') and (ext_trig_w_pulse = '1')) then --
                 clk_tp_cntr <= clk_tp_period_cnt;
                 clk_tp_out  <= '0';
                 cktp_done   <= '0';
             else
-                if ((ext_trigger_sim = '1') and (ext_trigger_in_sel = '1') and (vmm_cktp_en = '1') and (cktp_done = '0') and (ext_trig_w_pulse = '1')) then
+                if ((ext_trigger_port = '1') and (ext_trigger_in_sel = '1') and (vmm_cktp_en = '1') and (cktp_done = '0') and (ext_trig_w_pulse = '1')) then
                     if clk_tp_cntr = clk_tp_period_cnt then
                         clk_tp_cntr <= (others => '0');
                         clk_tp_out  <= '1';
@@ -1697,29 +1694,29 @@ begin
       reading_fin_flag_edge <= ((not reading_fin_flag_d) and reading_fin_flag);
     end process reading_fin_edge_detect;
 
-    ext_trig_edge_detect : process(clk_100, ext_trigger_sim, ext_trigger_d)
+    ext_trig_edge_detect : process(clk_100, ext_trigger_port, ext_trigger_d)
     begin
     --rising edge detect
       if rising_edge(clk_100) then
-        ext_trigger_d <= ext_trigger_sim;
+        ext_trigger_d <= ext_trigger_port;
       end if;
-      ext_trigger_edge <= (not(ext_trigger_d) and ext_trigger_sim);
+      ext_trigger_edge <= (not(ext_trigger_d) and ext_trigger_port);
     end process ext_trig_edge_detect;
 
-    ext_trig_edge_detect_slow : process(clk_40, ext_trigger_sim, ext_trigger_d_slow)
+    ext_trig_edge_detect_slow : process(clk_40, ext_trigger_port, ext_trigger_d_slow)
     begin
     --should sustain ext_trigger_edge_slow for 50 ns after cktp_done = '1'
       if rising_edge(clk_40) then
-            ext_trigger_d_slow <= ext_trigger_sim;     
+            ext_trigger_d_slow <= ext_trigger_port;     
       end if;
-      ext_trigger_edge_slow <= (not(ext_trigger_d_slow) and ext_trigger_sim);
+      ext_trigger_edge_slow <= (not(ext_trigger_d_slow) and ext_trigger_port);
     end process ext_trig_edge_detect_slow;
     
-    ext_trig_gate : process(clk_40, ext_trigger_sim)
+    ext_trig_gate : process(clk_40, ext_trigger_port)
     begin
     --rising edge detect
       if rising_edge(clk_40) then
-            ext_trigger_sync <= ext_trigger_sim;     
+            ext_trigger_sync <= ext_trigger_port;     
       end if;
     end process ext_trig_gate;
     
@@ -1728,9 +1725,7 @@ begin
     begin
       if rising_edge(clk_40) then
         if ext_trigger_in_sel = '1' then
---          if ext_trigger_sim = '1' then
---          if ext_trigger_edge_slow = '1' and cktp_done = '1' then
-          if ext_trigger_sim = '1' and cktp_done = '1' then
+          if ext_trigger_port = '1' and cktp_done = '1' then
             ext_trigger_flag <= '1';
             ext_trigger_delayed <= ext_trigger_flag;
           else
@@ -1738,8 +1733,6 @@ begin
             ext_trigger_delayed <= '0';
           end if;
         else
---          ext_trigger_sim <= '0';
---            ext_trigger_in_sw <= clk_ets_out;
           ext_trigger_flag <= '0';
         end if;
       end if;
