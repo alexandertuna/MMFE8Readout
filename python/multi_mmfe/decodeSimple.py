cf14fa6f8e32e424e071a384f9c1680e93f8ff5d
sezata <sezata@gmail.com>
2016-06-14 17:58:07 -0400

    fixed bugs

--- python/multi_mmfe/decodeSimple.py
+++ python/multi_mmfe/decodeSimple.py
@@ -22,36 +22,29 @@ def main(argv):
 
     datafile = open(inputfile, 'r')
     decodedfile = open(outputfile, 'w')
-    decodedfile.write('fifo_cnt \t bcid_trig \t num_trig \t channel \t PDO \t TDO \t BCID \t VMM \t MMFE8')
+    decodedfile.write('FIFO\tBCIDtrig\tNtrig\tCH\tPDO\tTDO\tBCID\tVMM\tMMFE8\n')
 #    num_trig = 0
     for line in datafile:
         thisline = line.split()
         if len(thisline) < 2:
             continue
         if thisline[2]=='!Err':
-            print "skip"
             continue
         fifocount = int(thisline[0])
         fifotrig = int(thisline[1], 16)
 #        if num_trig != int(fifotrig & 1048575):
 #            numwordsread = 0
         num_trig = int(fifotrig & 1048575)
-        print "num_trig: ",num_trig,"\n"
         fifotrig = fifotrig >> 20
         bcid_trig = int(fifotrig & 4095)
-        print "bcid_trig: ",bcid_trig,"\n"
-        print "thisline: ", thisline,"\n"
 #        linelength = 0
 #        for word in xrange(4,len(thisline)):
 #            if int(thisline[word],16) > 0:
 #                linelength = linelength + 1
 #        numwordsread = numwordsread + linelength
         for iword in xrange(4, (len(thisline)), 2): #get rid of peak command/address and fifo bcid/num trig
-            print "iword ",iword
-            print thisline[iword]
             word0 = int(thisline[iword],   16)
             word1 = int(thisline[iword+1], 16)
-            print word0
             if not word0 > 0:
                 print "Out of order or no data."
                 continue
@@ -74,7 +67,7 @@ def main(argv):
             word1 = word1 >> 4       # 4 bits of zeros?
             immfe = int(word1 & 255) # do we need to convert this?
 
-            decodedfile.write(str(fifocount) + '\t' + str(bcid_trig) + '\t' + str(num_trig) + '\t' + str(thisline[iword]) + '\t'+ str(addr) + '\t' + str(amp) + '\t' + str(timing) + '\t' + str(bcid_int) + str(vmm) + str(immfe))
+            decodedfile.write(str(fifocount) + '\t' + str(bcid_trig) + '\t' + str(num_trig) + '\t' + '\t'+ str(addr) + '\t' + str(amp) + '\t' + str(timing) + '\t' + str(bcid_int) + '\t'+ str(vmm) +'\t'+ str(immfe)+'\n')
             # to_print = "word0 = %s word1 = %s addr = %s amp = %s time = %s bcid = %s vmm = %s mmfe = %s"
             # Header = "fifo_cnt = %s bcid_trig = %s num_trig = %s "
             # decodedfile.write(header % (fifocount, bcid_trig, num_trig) + to_print % (thisline[iword], thisline[iword+1],
